d28b5215f0fac4da8d88c94074724d3e534df35d
Thai Thanh Hai <thaithanhhaick@gmail.com>
2025-10-28 22:01:23 +0700

    refactor: clean code

--- src/main/java/com/logitrack/order/service/implement/OrderServiceImplement.java
+++ src/main/java/com/logitrack/order/service/implement/OrderServiceImplement.java
@@ -2,10 +2,13 @@ package com.logitrack.order.service.implement;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.logitrack.order.dto.OrderRequest;
 import com.logitrack.order.dto.OrderResponse;
 import com.logitrack.order.service.OrderService;
 
+@Service
 public class OrderServiceImplement implements OrderService {
 
     @Override
